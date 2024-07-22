0d14420fa37a8fde6652966639ab4fc7023fb681
andrewtoby24 <thantkaung501@gmail.com>
2024-07-22 19:41:07 +0700

    CSS Project Day 4

--- CSS/CSS Final Project/Images/Apple iPhone 15 Pro Max specs - PhoneArena_files/overall-user-ratings-widget.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var p={p:"/var/www/phonearena.com/public_html/build/wp"};p.p})();
--- CSS/CSS Final Project/Images/Apple iPhone 15 Pro Max specs - PhoneArena_files/rules-p-PX0vauy7-Qh8f.js
+++
@@ -1,5 +0,0 @@
-/*
- Quantcast measurement tag
- Copyright (c) 2008-2022, Quantcast Corp.
-*/
-'use strict';(function(a,b,c){__qc("rules",[a])})("p-PX0vauy7-Qh8f",window,document);
